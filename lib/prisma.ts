bf55df1d4eeaa37901fc2866f2adc075c3993464
ZeroNoSho <inukuro351@gmail.com>
2026-09-23 10:02:05 +0700

    first commit

---
+++ lib/prisma.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@/generated/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST!,
+  port: Number(process.env.DATABASE_PORT ?? 3306),
+  user: process.env.DATABASE_USER!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_NAME!,
+});
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
